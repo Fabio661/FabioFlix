92719dacd7eebd2003d409f57b41e42cd168a8f1
Fabio Kauan <125137802+Fabio661@users.noreply.github.com>
2023-05-25 22:42:23 -0300

    Update settings.py

--- fabioflix/fabioflix/settings.py
+++ fabioflix/fabioflix/settings.py
@@ -18,13 +18,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config("SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = config("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = []
